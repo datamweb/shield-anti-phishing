c80719940f6652c704e07180ce2da41c9065101f
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2022-06-10 20:27:24 +0330

    add: blank files for `Shield Anti Phishing`

---
+++ src/Controllers/AntiPhishingController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Datamweb\AntiPhishing\Controllers;
+
+/**
+* --------------------------------------------------------------------------
+* CodeIgniter Shield Anti Phishing Controller.
+* --------------------------------------------------------------------------
+*
+* @package         shield-anti-phishing
+* @collection      Best Datamweb Tools CI4(BDT-CI4)
+* @author          Pooya Parsa Dadashi(@datamweb)
+* @link            https://datamweb.ir
+* @github_link     https://github.com/datamweb/shield-anti-phishing
+* @since           Version 1.0.0
+* @datepublic      2022-06-10 | 1401/03/20
+* 
+*/
+
+class AntiPhishingController extends BaseController
+{
+
+}
---
+++ tests/AntiPhishingTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Datamweb\AntiPhishing\Controllers;
+
+use CodeIgniter\Test\FeatureTestTrait;
+use Tests\Support\TestCase;
+use CodeIgniter\Test\CIUnitTestCase;
+
+/**
+* --------------------------------------------------------------------------
+* CodeIgniter Shield Anti Phishing Test.
+* --------------------------------------------------------------------------
+*
+* @package         shield-anti-phishing
+* @collection      Best Datamweb Tools CI4(BDT-CI4)
+* @author          Pooya Parsa Dadashi(@datamweb)
+* @link            https://datamweb.ir
+* @github_link     https://github.com/datamweb/shield-anti-phishing
+* @since           Version 1.0.0
+* @datepublic      2022-06-10 | 1401/03/20
+* 
+*/
+
+/**
+ * @internal
+ */
+final class AntiPhishingTest extends TestCase
+{
+
+}
